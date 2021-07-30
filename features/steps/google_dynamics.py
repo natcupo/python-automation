73e997872c30b56c85a783159c4408706a0dc402
Natalia Cupo <nataliacupo@natalias-air.home>
2021-07-30 08:45:17 -0400

    tests

--- features/steps/google_dynamics.py
+++ features/steps/google_dynamics.py
@@ -7,8 +7,9 @@ ELEMENTS = (By.XPATH, "//ul[@role='listbox']//li//div[@class='wM6W7d']")
 
 @when('Input {word} into text field')
 def enter_search_word(context, word):
-    search_word = context.driver.find_element(*SEARCH_WORD)
-    search_word.send_keys(word)
+    # search_word = context.driver.find_element(*SEARCH_WORD)
+    # search_word.send_keys(word)
+    context.application.main_page.google_dynamic(word)
 
 
 #@then('Forth element is shown')
@@ -25,14 +26,14 @@ def enter_search_word(context, word):
 
 @then('Second element is shown')
 def second_element(context):
-    el = context.driver.find_elements(*ELEMENTS)
-    print('Total elements are ' + str(len(el)))
+    #el = context.driver.find_elements(*ELEMENTS)
+    #print('Total elements are ' + str(len(el)))
     #print(el[2].text)
-    if el[2].text == "sephora syracuse ny":
-        print(el[2].text)
-    else:
-        print('error')
-
+    #if el[2].text == "sephora syracuse ny":
+        #print(el[2].text)
+    #else:
+        #print('error')
+    context.application.main_page.google_dynamics()
 
 
 
